67cc03561d406903fc1496d58ed6b67d10773ff2
user <ykchung@cityu.edu.hk>
2019-03-03 23:02:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationdrawer/FragmentFirst.java
@@ -0,0 +1,45 @@
+package com.example.myapplicationdrawer;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class FragmentFirst extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       // return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_first,null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(getActivity(),"You are inside Fragment 1", Toast.LENGTH_LONG);
+            }
+    } );
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
